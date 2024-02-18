fffb0a8f64306369758ee1ce9ce853691a6efaee
Swapnil-DevGeek <f20220181@goa.bits-pilani.ac.in>
2024-02-18 05:44:21 +0530

    ui majority done

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Dashboard from './Dashboard'
+function Home() {
+  return (
+    <div className='min-h-screen bg-[#161725] text-white'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,7 @@ import {
 } from "react-router-dom";
 import Login from './components/Login.jsx'
 import Signup from './components/Signup.jsx'
+import Home from './components/Home.jsx'
 
 const router = createBrowserRouter([
   {
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
   {
     path : "/signup",
     element : <Signup/>
+  },
+  {
+    path : "/home",
+    element : <Home/>
   }
 ]);
 
